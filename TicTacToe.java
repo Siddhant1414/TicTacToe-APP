d8eda5cf1c6d5e571a244d3744fbda0ad1a76d28
siddhantkhatri <khatrisiddhant996@gmail.com>
2026-04-24 10:33:06 +0530

    Added UC10: Draw condition logic

---
+++ TicTacToe.java
@@ -0,0 +1,23 @@
+public class TicTacToe {
+
+    static char[][] board = new char[3][3];   // ✅ MUST be here (top)
+
+    static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    static boolean checkDraw() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
